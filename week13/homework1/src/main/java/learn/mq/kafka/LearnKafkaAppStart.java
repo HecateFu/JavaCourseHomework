22c503870c87b84024637c1769268e2f0f8cf131
HecateFu <hecatefu@outlook.com>
2022-07-31 15:01:17 +0800

    作业1

---
+++ week13/homework1/src/main/java/learn/mq/kafka/LearnKafkaAppStart.java
@@ -0,0 +1,11 @@
+package learn.mq.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnKafkaAppStart {
+    public static void main(String[] args) {
+        SpringApplication.run(LearnKafkaAppStart.class,args);
+    }
+}
---
+++ week13/homework1/src/main/java/learn/mq/kafka/config/KafkaProperties.java
@@ -0,0 +1,14 @@
+package learn.mq.kafka.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "kafka")
+public class KafkaProperties {
+    private String bootstrapAddress;
+    private String topic;
+    private String consumerGroupId;
+}
